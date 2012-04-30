d83b2b50187570dcd9218eb4a186e41d102f4aff
Kanak Biscuitwala <kanak.b@hotmail.com>
2012-04-30 15:13:17 -0700

    An empty SocialKit app

---
+++ src/mobisocial/payments/PaymentsActivity.java
@@ -0,0 +1,13 @@
+package mobisocial.payments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PaymentsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
